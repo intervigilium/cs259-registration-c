709a13492fe3cb8816d036429ebb6605d14440f8
Ethan Chen <intervigil@gmail.com>
2011-03-01 18:26:45 -0800

    fix missing float

--- registration.h
+++ registration.h
@@ -21,7 +21,7 @@ void interp(float U1[M][N][P], float U2[M][N][P], float U3[M][N][P],
 	    float interpT[M][N][P]);
 
 void computeMI(float V1[M + 1][N + 1][P + 1], float V2[M + 1][N + 1][P + 1],
-	       V3[M + 1][N + 1][P + 1], float interpT[M][N][P],
+	       float V3[M + 1][N + 1][P + 1], float interpT[M][N][P],
 	       float S[M][N][P]);
 
 #endif
