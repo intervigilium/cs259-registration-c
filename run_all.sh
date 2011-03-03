694794e57777deea254c7c5bfc742730767dfa12
Ethan Chen <ethanc@cs.ucla.edu>
2011-03-03 00:17:47 -0800

    track getopt options parsing

--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-./registration 10 0
-./registration 10 1
-./registration 10 2
-./registration 10 3
-./registration 10 4
-./registration 10 5
-./registration 10 6
-./registration 10 7
-./registration 10 8
-./registration 10 9
+./registration -n 10 -b 0
+./registration -n 10 -b 1
+./registration -n 10 -b 2
+./registration -n 10 -b 3
+./registration -n 10 -b 4
+./registration -n 10 -b 5
+./registration -n 10 -b 6
+./registration -n 10 -b 7
+./registration -n 10 -b 8
+./registration -n 10 -b 9
